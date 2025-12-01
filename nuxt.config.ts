89a37147e858cd333023290d062e0bbe8ac67a63
YeThura-424 <68313967+YeThura-424@users.noreply.github.com>
2025-12-01 11:01:21 +0700

    add: tailwind and supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['./app/assets/css/main.css'],
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseAnonKey: process.env.SUPABASE_API_KEY,
+    }
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
