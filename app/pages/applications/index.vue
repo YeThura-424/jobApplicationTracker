30845abb6f49bd8a1a6f582b709bad8b42ab1c84
YeThura-424 <68313967+YeThura-424@users.noreply.github.com>
2025-12-13 23:01:56 +0700

    update for new application button

--- app/pages/applications/index.vue
+++ app/pages/applications/index.vue
@@ -6,8 +6,13 @@
         <h1 class="text-3xl md:text-4xl font-bold text-urban-darkslate">Job Applications</h1>
         <p class="text-urban-slate mt-2 text-sm md:text-base">Track and manage all your job applications</p>
       </div>
-      <NuxtLink to="/applications/new" class="btn-primary w-full sm:w-auto whitespace-nowrap py-2 md:py-3">
-        + New Application
+      <NuxtLink to="/applications/new"
+        class="btn-primary w-full flex gap-x-2 justify-center items-center sm:w-auto whitespace-nowrap py-2 md:py-3">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+          <path fill="currentColor"
+            d="M16 3C8.832 3 3 8.832 3 16s5.832 13 13 13s13-5.832 13-13S23.168 3 16 3m0 2c6.087 0 11 4.913 11 11s-4.913 11-11 11S5 22.087 5 16S9.913 5 16 5m-1 5v5h-5v2h5v5h2v-5h5v-2h-5v-5z" />
+        </svg>
+        <p>New Application</p>
       </NuxtLink>
     </div>
 
